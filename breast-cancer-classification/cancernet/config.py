2b89194014dde0903639e07e98de3659e48b4f67
Henry Smialowicz <henry.smialowicz@gmail.com>
2025-09-06 15:32:39 -0400

    Add training script, model definition, and config

--- breast-cancer-classification/cancernet/config.py
+++ breast-cancer-classification/cancernet/config.py
@@ -7,6 +7,5 @@ TRAIN_PATH = os.path.sep.join([BASE_PATH, "training"])
 VAL_PATH = os.path.sep.join([BASE_PATH, "validation"])
 TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
 
-#Set the percentages of entire data for training and validation
 TRAIN_SPLIT = 0.8
 VAL_SPLIT = 0.1
